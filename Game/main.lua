1a651548f86529cbd6d7693a1c0e2e4d9ce07718
ThatOneGuy2664 <158541271+ThatOneGuy2664@users.noreply.github.com>
2024-02-05 15:32:55 -0600

    Created main.lua

---
+++ Game/main.lua
@@ -0,0 +1,16 @@
+-- LUA allows for quicker updates, it will likely be done first.
+-- must be played in console.
+
+local PlayerClass = nil
+local PlayerRace = nil
+local PlayerLevel = 1
+local Races = {
+  "Human", "Dragonborn", "Dwarf",
+  "Elf", "Gnome", "Half-Elf", "Halfing",
+  "Half-Orc", "Tiefling"
+}
+local Classes = {
+  "Barbarian", "Bard", "Cleric", "Druid",
+  "Fighter", "Monk", "Paladin", "Ranger", "Rogue",
+  "Sorcerer", "Warlock", "Wizard", "Artificer", "Bloodhunter"
+}
