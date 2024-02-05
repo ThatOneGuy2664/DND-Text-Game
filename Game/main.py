965de367a00d14cdcc0353bab287255ab9b5a07d
ThatOneGuy2664 <158541271+ThatOneGuy2664@users.noreply.github.com>
2024-02-05 17:32:02 -0600

    Updater main.py

--- Game/main.py
+++ Game/main.py
@@ -21,6 +21,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 # END LICENSE DESCRIPTION
+# Python will have less updates but more eventual functionality.
 
 import random
 import time
