2fa34e4ae8b5877e2ac5cb6af3c60261260954e6
ThatOneGuy2664 <158541271+ThatOneGuy2664@users.noreply.github.com>
2024-02-05 20:28:00 -0600

    Updated main.lua

--- Game/main.lua
+++ Game/main.lua
@@ -171,6 +171,14 @@ WizardSpells = {
   Spells["Cantrips"][45],
   Spells["Cantrips"][46]
 }
+function rollDice(num_dice, num_sides)
+  local rolls = {}
+  for i = 1, num_dice do
+    table.insert(rolls, math.random(num_sides))
+  end
+  return rolls
+end -- (variable) = rollDice(2, 10) rolls 2 10 sided dice.
+
 
 if PlayerClass == "Rogue" then
   SneakAttack = true
