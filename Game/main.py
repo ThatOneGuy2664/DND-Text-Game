c0b2fd80e7f3f0133b70c9b1a53dfec2329864e4
ThatOneGuy2664 <158541271+ThatOneGuy2664@users.noreply.github.com>
2024-02-06 22:09:42 -0600

    Updated main.py

--- Game/main.py
+++ Game/main.py
@@ -2,7 +2,7 @@
 #  
 # Copyright (c) 2024 ThatOneGuy2664
 # Copyright (c) 2024 Twaddler01
-#
+# 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
