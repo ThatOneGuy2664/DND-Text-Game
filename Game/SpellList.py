e30541309352f9e7cf24eac75c9d28f23d8c9e36
ThatOneGuy2664 <158541271+ThatOneGuy2664@users.noreply.github.com>
2024-02-11 15:41:29 -0600

    Updated SpellList.py

--- Game/SpellList.py
+++ Game/SpellList.py
@@ -51,7 +51,7 @@ Spells = {
        "Vampiric Touch", "Wall Of Sand", "Wall Of Water", "Water Breathing", "Water Walk", "Wind Wall"
     ],
     "LevelFour": [
-       "Arcane Eye", "Aura Of Life", "Aura Of Purity", "Banishment", "Blight", "Charm Monster", "Compulsion", "Confusion"
+       "Arcane Eye", "Aura Of Life", "Aura Of Purity", "Banishment", "Blight", "Charm Monster", "Compulsion", "Confusion", "Conjure Barlgura", "Conjure Minor Elementals"
     ],
     "LevelFive": [],
     "LevelSix": [],
