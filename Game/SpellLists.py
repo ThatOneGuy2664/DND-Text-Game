fa211ef639955a75e89830e1f0f961fca4004db6
ThatOneGuy2664 <158541271+ThatOneGuy2664@users.noreply.github.com>
2024-02-11 21:22:49 -0600

    Created SpellLists.py

---
+++ Game/SpellLists.py
@@ -0,0 +1,17 @@
+# classes spell list
+BardSpells = {
+  "Cantrips":
+  "LevelOne":
+  "LevelTwo":
+  "LevelThree":
+}
+RangerSpells = {}
+WizardSpells = {}
+WarlockSpells = {}
+
+# schools
+EnchantmentSpells = {}
+AbjurationSpells = {}
+ConjurationSpells = {}
+NecromancySpells = {}
+EvocationSpells = {}
