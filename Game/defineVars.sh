4f72fff2f7e0159a59dbecebaf4f93d0b1754925
ThatOneGuy2664 <158541271+ThatOneGuy2664@users.noreply.github.com>
2024-02-11 20:50:38 -0600

    Updated defineVars.sh

--- Game/defineVars.sh
+++ Game/defineVars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run Lua script to generate cantrips.txt
-lua your_lua_script.lua
+lua FeatsList.lua
 
 # Run Python script to read from cantrips.txt
-python your_python_script.py
+python main.py
