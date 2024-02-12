c00f7d4be34736410784a9b92deec7bb99270b33
ThatOneGuy2664 <158541271+ThatOneGuy2664@users.noreply.github.com>
2024-02-11 20:42:04 -0600

    Updated FeatsList.lua

--- Game/FeatsList.lua
+++ Game/FeatsList.lua
@@ -1,7 +1,12 @@
-local str = "apple,banana,orange"
+-- Lua script to save the array to a file named "cantrips.txt"
+local str = "Actor,Alert,Artificer Initiate,Athlete"
 local result = {}
 for word in string.gmatch(str, "([^,]+)") do
     table.insert(result, word)
 end
--- result is now an array containing {"apple", "banana", "orange"}
--- this is an example
+
+local file = io.open("feats.txt", "w")
+for _, v in ipairs(result) do
+    file:write(v .. "\n")
+end
+file:close()
