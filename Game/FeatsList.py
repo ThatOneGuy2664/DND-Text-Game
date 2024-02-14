392c1daa5d6578c879c91df4077152483401e81d
ThatOneGuy2664 <158541271+ThatOneGuy2664@users.noreply.github.com>
2024-02-13 21:59:53 -0600

    Updated FeatsList.py

--- Game/FeatsList.py
+++ Game/FeatsList.py
@@ -23,4 +23,17 @@
 
 Feats = ["Alert", "Artificer Initiate", "Athlete", "Bountiful Luck", "Charger",
          "Chef", "Crossbow Expert", "Defensive Duelist", "Dragon Fear", "Dragon Hide",
-         "Drow High Magic", "Duel Wielder", "Dungeon Delver", "Durable", "Dwarf Fortitude"]
+         "Drow High Magic", "Duel Wielder", "Dungeon Delver", "Durable", "Dwarf Fortitude",
+         "Eldritch Adept", "Elemental Adept", "Elven Accuracy", "Ember Of The Fire Giant",
+         "Fade Away", "Fey Teleportation", "Fey Touched", "Fighting Initiate", "Flames Of Phlegethos",
+         "Fury Of The Frost Giants", "Gift Of The Chromatic Dragon", "Gift Of The Gem Dragon",
+         "Gift Of The Metallic Dragon", "Grappler", "Great Weapon Master", "Guile Of The Cloud Giant",
+         "Gunner", "Healer", "Heavily Armored", "Heavy Armor Master", "Infernal Constitution",
+         "Inspiring Leader", "Keen Mind", "Keenness Of The Stone Giant", "Lightly Armored",
+         "Linguist", "Lucky", "Mage Slayer", "Magic Initiate", "Martial Adept", "Medium Armor Master",
+         "Metamagic Adept", "Mobile", "Moderately Armored", "Mounted Combatant", "Observant", "Orcish Fury",
+         "Piercer", "Poisoner", "Polearm Master", "Prodigy", "Resilient", "Ritual Caster", "Rune Shaper",
+         "Savage Attacker", "Second Chance", "Sentinel", "Shadow Touched", "Sharpshooter", "Shield Master",
+         "Skill Expert", "Skilled", "Skulker", "Slasher", "Soul Of The Storm Giant", "Spell Sniper",
+         "Squat Nimbleness", "Strike Of The Giants", "Tavern Brawler", "Telekinetic", "Telepathic", "Tough",
+         "Vigor Of The Hill Giant", "War Caster", "Weapon Master", "Wood Elf Magic"]
